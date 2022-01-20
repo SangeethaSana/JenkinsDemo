5ffd11eacd465e464c74caa7019bfcae404047c5
Sangeetha <sangee199@gmail.com>
2022-01-20 18:42:34 +0530

    jenkin

---
+++ src/test/java/SampleProject/SampleTest.java
@@ -0,0 +1,12 @@
+package SampleProject;
+
+public class SampleTest {
+	
+	public static void Main (String[]args){
+		for(int i=0;i>=5;i++) {
+		
+		System.out.println("Welcome to world");
+		
+	}}
+
+}
