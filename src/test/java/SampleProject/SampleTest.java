b1fe012c99d08da4a2d7e9c148d22c04a1a8ecc7
Sangeetha <sangee199@gmail.com>
2022-01-20 18:51:18 +0530

    less

--- src/test/java/SampleProject/SampleTest.java
+++ src/test/java/SampleProject/SampleTest.java
@@ -1,9 +1,12 @@
 package SampleProject;
 
+import org.testng.annotations.Test;
+
 public class SampleTest {
 	
-	public static void Main (String[]args){
-		for(int i=0;i>=5;i++) {
+	@Test
+	public static void getouput() {
+		for(int i=0;i<=5;i++) {
 		
 		System.out.println("Welcome to world");
 		
